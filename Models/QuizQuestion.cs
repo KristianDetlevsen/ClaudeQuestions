0fca7b68df52c2c3322b1b10f4771717f099db08
Kristian Detlevsen <kbde63300@edu.ucl.dk>
2025-11-11 11:45:43 +0100

    Add project files.

---
+++ Models/QuizQuestion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ClaudeQuestions.Models
+{
+    public class QuizQuestion
+    {
+        [JsonPropertyName("difficulty")]
+        public required DifficultyLevel Difficulty { get; set; }
+        [JsonPropertyName("questionText")]
+        public required string QuestionText { get; set; }
+        [JsonPropertyName("options")]
+        public required List<string> Options { get; set; }
+        [JsonPropertyName("correctAnswerIndex")]
+        public required int CorrectAnswerIndex { get; set; }
+    }
+
+    public enum DifficultyLevel
+    {
+        Easy, Medium, Hard
+    }
+}
